24d4472fefa0d51ab339b168d0277bdcdedc545f
Jefersson Aldemar Velasquez Mendoza <jeferssonveme.97@gmail.com>
2024-08-11 14:49:54 -0500

    Efectos con useEffect

--- src/UseState.jsx
+++ src/UseState.jsx
@@ -1,11 +1,27 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 
 function UseState({name}){
 
     // Manejo de estado en funciones Utilizamos la siguiente sintaxis para declarar e inicializarr un estado y su modificador de valor.
 
-    const [ error, setError] = useState(true);
+    const [ error, setError] = useState(false);
+    const [ loading, setLoading] = useState(false)
+
+    useEffect(()=>{
+        if(!!loading){
+            setTimeout(()=>{
+                console.log("haciendo la validacion ")
+                setLoading(false)
+                console.log("terninado la validacion")
+            },3000)
+        }
+        console.log("terminado la validacion")
+    }, [loading]
+    // Array vacio. El código se ejecuta cuando se renderice por primera vez el componente.
+    // Ningún valor. El código se ejecuta cada vez que se haga render de nuestro componente o, dicho de de otra forma, cada vez que se modifique un estado.
+    // Array con elementos. Los estados que le pasemos al array harán que cuando haya un cambio en ellos se ejecutará la función.
+)
 
 
 
@@ -19,9 +35,12 @@ function UseState({name}){
             {error&& (<p>
                 Error EL codigo es incorrecto
             </p>)}
+            {loading && 
+                <p>Cargando ... </p>
+                }
             <input placeholder='Codigo de seguridad' />
             <button 
-                onClick={()=>setError(!error)}
+                onClick={()=>setLoading(true)}
             >combrobar</button>
         </div>
     );
