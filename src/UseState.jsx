6f62b3094cd219552fd59892de8a622c91a5c376
Jefersson Aldemar Velasquez Mendoza <jeferssonveme.97@gmail.com>
2024-08-11 22:30:17 -0500

    [200~Estados compuestos con useState

--- src/UseState.jsx
+++ src/UseState.jsx
@@ -8,26 +8,38 @@ function UseState({name}){
 
 
 
-    const [value, setValue] = useState("")
-    const [ error, setError] = useState(false);
-    const [ loading, setLoading] = useState(false)
-    console.log(value)
+    // const [value, setValue] = useState("")
+    // const [ error, setError] = useState(false);
+    // const [ loading, setLoading] = useState(false)
+    
+    const [state, setState] = useState({
+        value:"",
+        error:false,
+        loading: false,
+    })
+    
     useEffect(()=>{
-        if(!!loading){
+        if(!!state.loading){
             setTimeout(()=>{
                 console.log("haciendo la validacion ")
-                if(value === SECURITY_CODE){
-                    setLoading(false);
-                    setError(false);
+                if(state.value === SECURITY_CODE){
+                    setState({
+                        ...state,
+                        error:false,
+                        loading:false,
+                    })
                 }else{
-                    setError(true);
-                    setLoading(false);
+                    setState({
+                        ...state,
+                        error:true,
+                        loading:false,
+                    })
                 }
                 console.log("terninado la validacion")
             },3000)
         }
         console.log("terminado la validacion")
-    }, [loading]
+    }, [state.loading]
     // Array vacio. El código se ejecuta cuando se renderice por primera vez el componente.
     // Ningún valor. El código se ejecuta cada vez que se haga render de nuestro componente o, dicho de de otra forma, cada vez que se modifique un estado.
     // Array con elementos. Los estados que le pasemos al array harán que cuando haya un cambio en ellos se ejecutará la función.
@@ -42,21 +54,28 @@ function UseState({name}){
                 recibirlo como parámetro props el cual puede ser destructurado en las propiedades enviadas */}
             <h2> Eliminar {name}</h2>
             <p>por favor escribe el codigo de seguridad</p>
-            {(error && !loading) && (<p>
+            {(state.error && !state.loading) && (<p>
                 Error EL codigo es incorrecto
             </p>)}
-            {loading && 
+            {state.loading && 
                 <p>Cargando ... </p>
                 }
             <input placeholder='Codigo de seguridad' 
-                value={value}
+                value={state.value}
                 onChange={(event) => {
                     // setError(false)
-                    setValue(event.target.value)
+                    setState({
+                        ...state,
+                        value:event.target.value
+                    })
                 }}
             />
             <button 
-                onClick={()=>setLoading(true)}
+                onClick={()=>setState({
+                    ...state,
+                    loading:true,
+                }
+                )}
             >combrobar</button>
         </div>
     );
